3d630d65960dee0926994cdd933f14b24e35af90
Eirik Mildestveit Hammerstad <emh@dips.no>
2015-04-22 21:42:00 +0200

    Ignore dotsettings and minor changes.

--- Moya/Attributes/RunTimesAttribute.cs
+++ Moya/Attributes/RunTimesAttribute.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
 
 namespace Moya.Attributes
 {
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-    [SuppressMessage("Microsoft.Performance", "CA1813:AvoidUnsealedAttributes", Justification = "This attribute is designed as an extensibility point.")]
-    
-    class RunTimesAttribute : Attribute
+     class RunTimesAttribute : Attribute
     {
         int _times;
 
