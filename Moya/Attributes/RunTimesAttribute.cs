bed16c05963492043a9850fcd74a6ed10e443ae8
Eirik Mildestveit Hammerstad <emh@dips.no>
2015-04-22 21:38:02 +0200

    Added attribute and testproject.

---
+++ Moya/Attributes/RunTimesAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Moya.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    [SuppressMessage("Microsoft.Performance", "CA1813:AvoidUnsealedAttributes", Justification = "This attribute is designed as an extensibility point.")]
+    
+    class RunTimesAttribute : Attribute
+    {
+        int _times;
+
+        public RunTimesAttribute(int times)
+        {
+            _times = times;
+        }
+    }
+}
