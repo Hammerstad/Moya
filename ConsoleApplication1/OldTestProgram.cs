c251305be418c60b40dcd1ec14d2862671572c03
Eirik Mildestveit Hammerstad <emh@dips.no>
2015-07-29 15:34:35 +0200

    Comments out OldTestProgram.cs

--- ConsoleApplication1/OldTestProgram.cs
+++ ConsoleApplication1/OldTestProgram.cs
@@ -1,72 +1,72 @@
-﻿using System;
-using System.Reflection;
-using Moya.Attributes;
-using Moya.Runner.Runners;
-using Moya.Runner.Statistics;
+﻿//using System;
+//using System.Reflection;
+//using Moya.Attributes;
+//using Moya.Runner.Runners;
+//using Moya.Runner.Statistics;
 
-namespace ConsoleApplication1
-{
-    public class OldTestProgram
-    {
+//namespace ConsoleApplication1
+//{
+//    public class OldTestProgram
+//    {
 
-        public static int Add(int a, int b)
-        {
-            return a + b;
-        }
+//        public static int Add(int a, int b)
+//        {
+//            return a + b;
+//        }
 
-        public static void Add(int a, int b, int c)
-        {
-        }
+//        public static void Add(int a, int b, int c)
+//        {
+//        }
 
-        [Stress(Users = 2, Times = 5)]
-        public void Test123()
-        {
+//        [Stress(Users = 2, Times = 5)]
+//        public void Test123()
+//        {
 
-        }
+//        }
 
-        public static void PrintLoadTestInfo(MemberInfo t)
-        {
+//        public static void PrintLoadTestInfo(MemberInfo t)
+//        {
 
-            // Using reflection.
-            System.Attribute[] attrs = System.Attribute.GetCustomAttributes(t);  // Reflection. 
+//            // Using reflection.
+//            System.Attribute[] attrs = System.Attribute.GetCustomAttributes(t);  // Reflection. 
 
-            // Displaying output. 
-            foreach (System.Attribute attr in attrs)
-            {
-                if (attr is StressAttribute)
-                {
-                    StressAttribute a = (StressAttribute)attr;
-                    System.Console.WriteLine("Author information for {0}", t);
-                    System.Console.WriteLine("Times: {0} \t::\t Runners: {1}", a.Times, a.Users);
-                }
-            }
-        }
+//            // Displaying output. 
+//            foreach (System.Attribute attr in attrs)
+//            {
+//                if (attr is StressAttribute)
+//                {
+//                    StressAttribute a = (StressAttribute)attr;
+//                    System.Console.WriteLine("Author information for {0}", t);
+//                    System.Console.WriteLine("Times: {0} \t::\t Runners: {1}", a.Times, a.Users);
+//                }
+//            }
+//        }
 
-        public static void TestMethod1()
-        {
-            var decoratedLoadTestRunner = new TimerDecorator(new StressTestRunner
-            {
-                Runners = 100,
-                Times = 100
-            });
+//        public static void TestMethod1()
+//        {
+//            var decoratedLoadTestRunner = new TimerDecorator(new StressTestRunner
+//            {
+//                Users = 100,
+//                Times = 100
+//            });
 
-            Action action = () => Add(2, 3, 4);
-            Func<int> function = () => Add(2, 3);
-            decoratedLoadTestRunner.Execute(action);
-            decoratedLoadTestRunner.Execute(function);
+//            Action action = () => Add(2, 3, 4);
+//            Func<int> function = () => Add(2, 3);
+//            decoratedLoadTestRunner.Execute(action);
+//            decoratedLoadTestRunner.Execute(function);
 
-            Console.WriteLine(DurationManager.DefaultInstance.GetDurationFromHashcode(action.GetHashCode()));
-            Console.WriteLine(DurationManager.DefaultInstance.GetDurationFromHashcode(function.GetHashCode()));
+//            Console.WriteLine(DurationManager.DefaultInstance.GetDurationFromHashcode(action.GetHashCode()));
+//            Console.WriteLine(DurationManager.DefaultInstance.GetDurationFromHashcode(function.GetHashCode()));
 
-            Console.WriteLine("\n\n");
+//            Console.WriteLine("\n\n");
 
-            MethodInfo[] methods = typeof(Program).GetMethods();
-            foreach (var method in methods)
-            {
-                PrintLoadTestInfo(method);
-            }
+//            MethodInfo[] methods = typeof(Program).GetMethods();
+//            foreach (var method in methods)
+//            {
+//                PrintLoadTestInfo(method);
+//            }
 
-            Console.Read();
-        } 
-    }
-}
+//            Console.Read();
+//        } 
+//    }
+//}
