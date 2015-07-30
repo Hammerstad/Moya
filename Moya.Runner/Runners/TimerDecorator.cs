f345ede698fb8ac51480ff30f7b5a9ecb73eaddf
Eirik Mildestveit Hammerstad <emh@dips.no>
2015-07-30 14:03:14 +0200

    Remove reference to statistic manager from timerdecorator.

--- Moya.Runner/Runners/TimerDecorator.cs
+++ Moya.Runner/Runners/TimerDecorator.cs
@@ -3,18 +3,14 @@
     using System.Diagnostics;
     using System.Reflection;
     using Models;
-    using Statistics;
 
     public class TimerDecorator : ITestRunner
     {
-        private readonly IDurationManager duration;
-
         public ITestRunner DecoratedTestRunner { get; set; }
         
         public TimerDecorator(ITestRunner testRunner)
         {
             DecoratedTestRunner = testRunner;
-            duration = DurationManager.DefaultInstance;
         }
 
         public ITestResult Execute(MethodInfo methodInfo)
@@ -25,7 +21,6 @@
             var result = DecoratedTestRunner.Execute(methodInfo);
 
             stopwatch.Stop();
-            duration.AddOrUpdateDuration(methodInfo.GetHashCode(), stopwatch.ElapsedMilliseconds);
 
             result.Duration = stopwatch.ElapsedMilliseconds;
             return result;
