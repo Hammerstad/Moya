510ffce1877f2cb7c7e1900764970a5838e7ff46
Eirik Mildestveit Hammerstad <emh@dips.no>
2015-07-30 14:38:23 +0200

    Handle optionargumentpairs with only option

--- Moya.Runner.Console/OptionArgumentPair.cs
+++ Moya.Runner.Console/OptionArgumentPair.cs
@@ -1,5 +1,7 @@
 ﻿namespace Moya.Runner.Console
 {
+    using System.Linq;
+
     public struct OptionArgumentPair
     {
         public string Option { get; set; }
@@ -9,10 +11,11 @@
         public static OptionArgumentPair Create(string stringFromCommandLine)
         {
             string[] optionAndArgument = stringFromCommandLine.Split('=');
+
             return new OptionArgumentPair
             {
-                Option = optionAndArgument[0],
-                Argument = optionAndArgument[1]
+                Option = optionAndArgument.Any() ? optionAndArgument[0] : string.Empty,
+                Argument = optionAndArgument.Count() > 1 ? optionAndArgument[1] : string.Empty
             };
         }
     }
