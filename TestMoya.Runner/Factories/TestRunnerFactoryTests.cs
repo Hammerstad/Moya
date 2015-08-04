fe8c20594de8922444cc8164b0632d0ac9c8236f
Eirik Mildestveit Hammerstad <emh@dips.no>
2015-08-04 21:29:17 +0200

    Add TestRunnerFactoryTests

---
+++ TestMoya.Runner/Factories/TestRunnerFactoryTests.cs
@@ -0,0 +1,28 @@
+﻿namespace TestMoya.Runner.Factories
+{
+    using Moya.Attributes;
+    using Moya.Runner.Factories;
+    using Moya.Runner.Runners;
+    using Xunit;
+
+    public class TestRunnerFactoryTests
+    {
+        private readonly ITestRunnerFactory testRunnerFactory = new TestRunnerFactory();
+
+        [Fact]
+        public void GetTestRunnerForStressAttributeReturnsTimerDecorator()
+        {
+            ITestRunner testRunner = testRunnerFactory.GetTestRunnerForAttribute(typeof(StressAttribute));
+
+            Assert.Equal(typeof(TimerDecorator), testRunner.GetType());
+        }
+
+        [Fact]
+        public void GetTestRunnerForStressAttributeReturnsTimerDecoratorContainingStressTestRunner()
+        {
+            ITestRunner testRunner = testRunnerFactory.GetTestRunnerForAttribute(typeof(StressAttribute));
+
+            Assert.Equal(typeof(StressTestRunner), ((TimerDecorator)testRunner).DecoratedTestRunner.GetType());
+        }
+    }
+}
