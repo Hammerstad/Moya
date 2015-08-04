2df4ea0b66205f286cb00beea71794413d1a0586
Eirik Mildestveit Hammerstad <emh@dips.no>
2015-08-04 13:38:45 +0200

    Add dictionary extension for addrange

---
+++ Moya/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Moya.Extensions
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public static class DictionaryExtensions
+    {
+        public static void AddRange<TKey, TValue>(this IDictionary<TKey, TValue> source, IDictionary<TKey, TValue> collection)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException("collection");
+            }
+
+            foreach (var item in collection.Where(item => !source.ContainsKey(item.Key)))
+            {
+                source.Add(item.Key, item.Value);
+            }
+        }
+    }
+}
