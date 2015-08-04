eb75aa60ccca0b073909b1776b4a4abe2fe378ec
Eirik Mildestveit Hammerstad <emh@dips.no>
2015-08-04 15:46:12 +0200

    Add string extensions tests

---
+++ TestMoya/Extensions/StringExtensionsTests.cs
@@ -0,0 +1,38 @@
+﻿using Moya.Extensions;
+using Xunit;
+
+namespace TestMoya.Extensions
+{
+    public class StringExtensionsTests
+    {
+        [Fact]
+        public void StringExtensionsFormatWithWorksWithWorksWithStrings()
+        {
+            const string Expected = "Hello world!";
+
+            var actual = "{0} {1}".FormatWith("Hello", "world!");
+
+            Assert.Equal(Expected, actual);
+        }
+
+        [Fact]
+        public void StringExtensionsFormatWithWorksWithWorksWithIntegers()
+        {
+            const string Expected = "1 2 3 4";
+
+            var actual = "{0} {1} {2} {3}".FormatWith(1, 2, 3, 4);
+
+            Assert.Equal(Expected, actual);
+        }
+
+        [Fact]
+        public void StringExtensionsFormatWithWorksWithWorksWithIntegersAndStrings()
+        {
+            const string Expected = "1 2 Hello World!";
+
+            var actual = "{0} {1} {2} {3}".FormatWith(1, 2, "Hello", "World!");
+
+            Assert.Equal(Expected, actual);
+        }
+    }
+}
