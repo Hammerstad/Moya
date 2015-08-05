0d746e47d65eb5cdc6cbc5a63c490d2875d3674b
Eirik Mildestveit Hammerstad <emh@dips.no>
2015-08-05 09:15:32 +0200

    Add tests for TimerDecorator

---
+++ TestMoya.Runner/Runners/TimerDecoratorTests.cs
@@ -0,0 +1,52 @@
+﻿namespace TestMoya.Runner.Runners
+{
+    using System;
+    using System.Reflection;
+    using System.Threading;
+    using Moq;
+    using Moya.Models;
+    using Moya.Runner.Runners;
+    using Xunit;
+
+    public class TimerDecoratorTests
+    {
+        private readonly Mock<ITestRunner> testRunnerMock;
+        private readonly TimerDecorator timerDecorator;
+
+        public TimerDecoratorTests()
+        {
+            testRunnerMock = new Mock<ITestRunner>();
+            timerDecorator = new TimerDecorator(testRunnerMock.Object);
+        }
+
+        [Fact]
+        public void TimerDecoratorExecuteRunsMethod()
+        {
+            bool methodRun = false;
+            MethodInfo method = ((Action)(() => methodRun = true)).Method;
+            testRunnerMock
+                .Setup(x => x.Execute(method))
+                .Callback(() => methodRun = true)
+                .Returns(new TestResult());
+
+            timerDecorator.Execute(method);
+
+            Assert.True(methodRun);
+        }
+
+        [Fact]
+        public void TimerDecoratorExecuteAddsDurationToResult()
+        {
+            bool methodRun = false;
+            MethodInfo method = ((Action)(() => Thread.Sleep(1))).Method;
+            testRunnerMock
+                .Setup(x => x.Execute(method))
+                .Callback(() => methodRun = true)
+                .Returns(new TestResult());
+
+            var result = timerDecorator.Execute(method);
+
+            Assert.True(result.Duration > 0);
+        }
+    }
+}
