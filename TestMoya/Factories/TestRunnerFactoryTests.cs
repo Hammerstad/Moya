83443b636c0a12b3598f546de7d427199b953950
Eirik Mildestveit Hammerstad <emh@dips.no>
2015-08-14 14:19:14 +0200

    Add TestRunnerFactory tests for newly added attributes

--- TestMoya/Factories/TestRunnerFactoryTests.cs
+++ TestMoya/Factories/TestRunnerFactoryTests.cs
@@ -9,32 +9,50 @@
     public class TestRunnerFactoryTests
     {
         private readonly ITestRunnerFactory testRunnerFactory = new TestRunnerFactory();
-
-        [Fact]
-        public void GetTestRunnerForStressAttributeReturnsTimerDecorator()
-        {
-            IMoyaTestRunner testRunner = testRunnerFactory.GetTestRunnerForAttribute(typeof(StressAttribute));
-
-            Assert.Equal(typeof(TimerDecorator), testRunner.GetType());
-        }
-
+        
         [Fact]
         public void GetTestRunnerForStressAttributeReturnsTimerDecoratorContainingStressTestRunner()
         {
             IMoyaTestRunner testRunner = testRunnerFactory.GetTestRunnerForAttribute(typeof(StressAttribute));
 
+            Assert.Equal(typeof(TimerDecorator), testRunner.GetType());
             Assert.Equal(typeof(StressTestRunner), ((TimerDecorator)testRunner).DecoratedTestRunner.GetType());
         }
 
         [Fact]
         public void GetTestRunnerForInvalidAttributeThrowsMoyaException()
         {
-            IMoyaTestRunner testRunner;
-            
             var exception = Record.Exception(() => testRunnerFactory.GetTestRunnerForAttribute(typeof(MoyaAttribute)));
 
             Assert.Equal(typeof(MoyaException), exception.GetType());
             Assert.Equal("Unable to provide moya test runner for type Moya.Attributes.MoyaAttribute", exception.Message);
+        }
+
+        [Fact]
+        public void GetTestRunnerForWarmupAttributeReturnsTimerDecoratorContainingWarmupTestRunner()
+        {
+            IMoyaTestRunner testRunner = testRunnerFactory.GetTestRunnerForAttribute(typeof(WarmupAttribute));
+
+            Assert.Equal(typeof(TimerDecorator), testRunner.GetType());
+            Assert.Equal(typeof(WarmupTestRunner), ((TimerDecorator)testRunner).DecoratedTestRunner.GetType());
+        }
+
+        [Fact]
+        public void GetTestRunnerForLongerThanAttributeReturnsTimerDecoratorContainingLongerThanTestRunner()
+        {
+            IMoyaTestRunner testRunner = testRunnerFactory.GetTestRunnerForAttribute(typeof(LongerThanAttribute));
+
+            Assert.Equal(typeof(TimerDecorator), testRunner.GetType());
+            Assert.Equal(typeof(LongerThanTestRunner), ((TimerDecorator)testRunner).DecoratedTestRunner.GetType());
+        }
+
+        [Fact]
+        public void GetTestRunnerForLessThanAttributeReturnsTimerDecoratorContainingLessThanTestRunner()
+        {
+            IMoyaTestRunner testRunner = testRunnerFactory.GetTestRunnerForAttribute(typeof(LessThanAttribute));
+
+            Assert.Equal(typeof(TimerDecorator), testRunner.GetType());
+            Assert.Equal(typeof(LessThanTestRunner), ((TimerDecorator)testRunner).DecoratedTestRunner.GetType());
         }
     }
 }
