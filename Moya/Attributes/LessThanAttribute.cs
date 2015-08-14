72383edffc241b70ed0ab28954e9d91849d8558a
Eirik Mildestveit Hammerstad <emh@dips.no>
2015-08-14 14:04:12 +0200

    Add LessThanAttribute

---
+++ Moya/Attributes/LessThanAttribute.cs
@@ -0,0 +1,20 @@
+﻿namespace Moya.Attributes
+{
+    using System;
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class LessThanAttribute : MoyaAttribute
+    {
+        public LessThanAttribute(int seconds)
+        {
+            Seconds = seconds;
+        }
+
+        public LessThanAttribute()
+        {
+
+        }
+
+        public int Seconds { get; set; }
+    }
+}
