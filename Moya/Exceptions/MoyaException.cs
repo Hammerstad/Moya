f32780c7865d8f158d93f3f7e89b6e0ff7d4201a
Eirik Mildestveit Hammerstad <emh@dips.no>
2015-08-17 14:16:57 +0200

    Add xml documentation to moya exceptions

--- Moya/Exceptions/MoyaException.cs
+++ Moya/Exceptions/MoyaException.cs
@@ -2,11 +2,19 @@
 {
     using System;
 
+    /// <summary>
+    /// Represents errors that occur during execution of Moya.
+    /// </summary>
     public class MoyaException : Exception
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MoyaException"/> class
+        /// with a specified error message.
+        /// </summary>
+        /// <param name="message">A message describing the error.</param>
         public MoyaException(String message) : base(message)
         {
-            
+
         }
     }
 }
